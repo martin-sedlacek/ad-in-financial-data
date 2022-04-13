a32d32cb58e36265eb22725e3ca3e840edc4ff88
Martin Sedlacek <martin.martin.sedlacek@gmail.com>
2022-04-13 01:20:40 +0100

    Add original EMMV method for non-torch data

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -36,6 +36,54 @@ def mass_volume(axis_alpha, volume_support, s_unif, s_X, n_generated):
     return auc(axis_alpha, mv), mv
 
 
+def emmv_scores(trained_model, df, scoring_func=None, n_generated=10000, alpha_min=0.9, alpha_max=0.999, t_max=0.9):
+    """Get Excess-Mass (EM) and Mass Volume (MV) scores for unsupervised ML AD models.
+    :param trained_model: Trained ML model with a 'decision_function' method
+    :param df: Pandas dataframe of features (X matrix)
+    :param scoring_func: Anomaly scoring function (callable)
+    :param n_generated: , defaults to 10000
+    :param alpha_min: Min value for alpha axis, defaults to 0.9
+    :param alpha_max: Max value for alpha axis, defaults to 0.999
+    :param t_max: Min EM value required, defaults to 0.9
+    :return: A dictionary of two scores ('em' and 'mv')
+    """
+
+    if scoring_func is None:
+        scoring_func = lambda model, df: model.decision_function(df)
+
+    # Get limits and volume support.
+    lim_inf = df.min(axis=0)
+    lim_sup = df.max(axis=0)
+    print(lim_inf.shape)
+    offset = 1e-60 # to prevent division by 0
+    try:
+        volume_support = (lim_sup - lim_inf).prod() + offset
+    except AttributeError as e: # i.e Pandas Series
+        volume_support = (lim_sup - lim_inf) + offset
+    print(volume_support)
+    # Determine EM and MV parameters
+    t = np.arange(0, 100 / volume_support, 0.01 / volume_support)
+    axis_alpha = np.arange(alpha_min, alpha_max, 0.0001)
+    try:
+        unif = np.random.uniform(lim_inf, lim_sup, size=(n_generated, df.shape[1]))
+    except IndexError as e: # i.e. 1D data
+        unif = np.random.uniform(lim_inf, lim_sup, size=(n_generated))
+
+    # Get anomaly scores
+    anomaly_score = scoring_func(trained_model, df)#.reshape(1, -1)[0]
+    s_unif = scoring_func(trained_model, unif)
+
+    # Get EM and MV scores
+    AUC_em, em, amax = excess_mass(t, t_max, volume_support, s_unif, anomaly_score, n_generated)
+    AUC_mv, mv = mass_volume(axis_alpha, volume_support, s_unif, anomaly_score, n_generated)
+    # Return a dataframe containing EMMV information
+    scores = {
+        'em': np.mean(em),
+        'mv': np.mean(mv),
+    }
+    return scores
+
+
 def torch_emmv_scores(trained_model, x, scoring_func=None, n_generated=10000, alpha_min=0.9, alpha_max=0.999,
                       t_max=0.9):
     # Get limits and volume support.
