4dc6ca65b3dfb1e8a8a8567884918a80b7c247b4
Martin Sedlacek <martin.martin.sedlacek@gmail.com>
2022-04-16 18:20:15 +0100

    Add DeepAnT CNN and LSTM

--- models/DeepAnT.py
+++ models/DeepAnT.py
@@ -1,9 +1,9 @@
 import torch
+import numpy as np
 
-
-class DeepAnT(torch.nn.Module):
+class DeepAnT_CNN(torch.nn.Module):
     def __init__(self, LOOKBACK_SIZE, DIMENSION, anomaly_threshold):
-        super(DeepAnT, self).__init__()
+        super(DeepAnT_CNN, self).__init__()
         self.anomaly_threshold = anomaly_threshold
         self.conv1d_1_layer = torch.nn.Conv1d(in_channels=LOOKBACK_SIZE, out_channels=64, kernel_size=1)
         self.relu_1_layer = torch.nn.ReLU()
@@ -30,3 +30,35 @@ class DeepAnT(torch.nn.Module):
         x = self.dropout_layer(x)
         x = self.dense_2_layer(x)
         return x
+
+    @staticmethod
+    def anomaly_detector(prediction_seq, ground_truth_seq, anm_det_thr):
+        # calculate Euclidean between actual seq and predicted seq
+        # ist = np.linalg.norm(ground_truth_seq - prediction_seq)
+        dist = np.absolute(ground_truth_seq - prediction_seq).mean(axis=prediction_seq.ndim-1)
+        return (dist > anm_det_thr).astype(float)
+
+
+class DeepAnT_LSTM(torch.nn.Module):
+    def __init__(self, LOOKBACK_SIZE, DIMENSION, anomaly_threshold):
+        super(DeepAnT_LSTM, self).__init__()
+        self.anomaly_threshold = anomaly_threshold
+        self.lstm_1_layer = torch.nn.LSTM(DIMENSION, 128, 2)
+        self.dropout_1_layer = torch.nn.Dropout(p=0.2)
+        self.lstm_2_layer = torch.nn.LSTM(128, 64, 2)
+        self.dropout_2_layer = torch.nn.Dropout(p=0.2)
+        self.linear_layer = torch.nn.Linear(64, DIMENSION)
+
+    def forward(self, x):
+        x, (_, _) = self.lstm_1_layer(x)
+        x = self.dropout_1_layer(x)
+        x, (_, _) = self.lstm_2_layer(x)
+        x = self.dropout_2_layer(x)
+        return self.linear_layer(x)
+
+    @staticmethod
+    def anomaly_detector(prediction_seq, ground_truth_seq, anm_det_thr):
+        # calculate Euclidean between actual seq and predicted seq
+        # ist = np.linalg.norm(ground_truth_seq - prediction_seq)
+        dist = np.absolute(ground_truth_seq - prediction_seq).mean(axis=prediction_seq.ndim-1)
+        return (dist > anm_det_thr).astype(float)
--- training/DeepAnT_train.py
+++ training/DeepAnT_train.py
@@ -1,27 +1,18 @@
-import numpy as np
 import torch
 from utils import evaluation
+import numpy as np
 
 
 class DeepAntTrainingPipeline():
     @staticmethod
-    def anomaly_detector(prediction_seq, ground_truth_seq, anm_det_thr):
-        # calculate Euclidean between actual seq and predicted seq
-        # ist = np.linalg.norm(ground_truth_seq - prediction_seq)
-        dist = np.absolute(ground_truth_seq - prediction_seq).mean(axis=1)
-        return (dist > anm_det_thr).astype(float)
-
-    @staticmethod
-    def torch_scoring_function(model, data):
-        return model(data)
-
-    @staticmethod
-    def train_epoch(model, loss_fn, optimizer, train_dl):
+    def train_epoch(model, loss_fn, optimizer, train_dl, DEVICE):
         loss_sum = 0.0
         ctr = 0
-        for x_batch, y_batch, next_batch in train_dl:
+        for x_batch, y_batch, next_batch, next_batch_labels in train_dl:
+            x_batch = x_batch.to(DEVICE)
+            next_batch = next_batch.to(DEVICE)
             model.train()
-            pred_next = model(x_batch)
+            pred_next = model(x_batch).to(DEVICE)
             loss = loss_fn(next_batch, pred_next)
             loss.backward()
             optimizer.step()
@@ -31,27 +22,90 @@ class DeepAntTrainingPipeline():
             ctr += 1
         return float(loss_sum / ctr)
 
-    def train(self, model, loss, optimizer, train_dl, test_dl, num_epochs, per_epoch_evaluation=False):
+    def train(self, model, loss, optimizer, train_dl, test_dl, num_epochs, DEVICE, per_epoch_evaluation=False):
+        model = model.to(DEVICE)
         for epoch in range(num_epochs):
-            epoch_train_loss = self.train_epoch(model, loss, optimizer, train_dl)
+            epoch_train_loss = self.train_epoch(model, loss, optimizer, train_dl, DEVICE)
             print("Training Loss: {0} - Epoch: {1}".format(epoch_train_loss, epoch + 1))
             if per_epoch_evaluation:
-                test_acc, test_em, test_mv = self.evaluate(model, test_dl)
+                test_acc, test_em, test_mv = self.evaluate(model, test_dl, DEVICE)
                 print("Acc: {0}, EM: {1}, MV: {2} - Epoch: {3}".format(test_acc, test_em, test_mv, epoch + 1))
+
+    @staticmethod
+    def deepant_emmv(trained_model, pre_x, x, scoring_func=None, n_generated=10000, alpha_min=0.9, alpha_max=0.999,t_max=0.9):
+        # Get limits and volume support.
+        lim_inf = torch.min(x.view(-1, 6), dim=0)[0]
+        lim_sup = torch.max(x.view(-1, 6), dim=0)[0]
+        offset = 1e-60  # to prevent division by 0
+
+        # Volume support
+        volume_support = torch.prod(lim_sup - lim_inf).item() + offset
+
+        # Determine EM and MV parameters
+        t = np.arange(0, 100 / volume_support, 0.01 / volume_support)
+        axis_alpha = np.arange(alpha_min, alpha_max, 0.0001)
+
+        unif = torch.rand(n_generated, x.size(1), x.size(2))
+        m = lim_sup - lim_inf
+        unif = unif * m
+        unif = unif + lim_inf
+
+        # Generate pre_unif
+        lim_inf = torch.min(pre_x.view(-1, 6), dim=0)[0]
+        lim_sup = torch.max(pre_x.view(-1, 6), dim=0)[0]
+        volume_support = torch.prod(lim_sup - lim_inf).item() + offset
+        t = np.arange(0, 100 / volume_support, 0.01 / volume_support)
+        axis_alpha = np.arange(alpha_min, alpha_max, 0.0001)
+        pre_unif = torch.rand(n_generated, pre_x.size(1), pre_x.size(2))
+        m = lim_sup - lim_inf
+        pre_unif = pre_unif * m
+        pre_unif = pre_unif + lim_inf
+
+        # Get anomaly scores
+        anomaly_score = scoring_func(trained_model, pre_x, x).flatten()
+        s_unif = scoring_func(trained_model, pre_unif, unif).flatten()
+
+        # Get EM and MV scores
+        AUC_em, em, amax = evaluation.excess_mass(t, t_max, volume_support, s_unif, anomaly_score, n_generated)
+        AUC_mv, mv = evaluation.mass_volume(axis_alpha, volume_support, s_unif, anomaly_score, n_generated)
+
+        # Return a dataframe containing EMMV information
+        scores = {
+            'em': np.mean(em),
+            'mv': np.mean(mv),
+        }
+        return scores
 
     @torch.no_grad()
     def evaluate(self, model, test_dl):
+        total_em = total_mv = total_acc = total_precision = total_recall = 0
         model.eval()
-        total_acc = total_em = total_mv = 0
-        ctr = 0
-        for X, Y, P in test_dl:
-            pred = model(X).detach()
-            anomaly_predict = torch.tensor(self.anomaly_detector(pred.numpy(), P.numpy(), model.anomaly_threshold))
-            anomaly_label = Y.squeeze()
-            acc = evaluation.accuracy(anomaly_predict, anomaly_label)
+        model = model.cpu()
+        for X, Y, P, PL in test_dl:
+            seq_prediction = model(X).detach()
+            label_prediction = torch.tensor(model.anomaly_detector(seq_prediction.numpy(), P.numpy(), model.anomaly_threshold))
+
+            true_positives, true_negatives, false_positives, false_negatives = evaluation.metric_calc(label_prediction, PL)
+            acc = (true_positives + true_negatives) / (PL.size(0) * PL.size(1))
             total_acc += acc
-            ctr += 1
-            scores = evaluation.torch_emmv_scores(model, X, scoring_func=self.torch_scoring_function)
+            if true_positives + false_positives > 0:
+                precision = true_positives / (true_positives + false_positives)
+                total_precision += precision
+            if true_positives + false_negatives > 0:
+                recall = true_positives / (true_positives + false_negatives)
+                total_recall += recall
+
+            scores = self.deepant_emmv(model, X, P, scoring_func=self.scoring_function)
+            print(scores)
+
             total_em += scores['em']
             total_mv += scores['mv']
-        return total_acc / ctr, total_em / ctr, total_mv / ctr
+        print(total_em / len(test_dl), total_mv / len(test_dl))
+        print(total_acc / len(test_dl), total_precision / len(test_dl), total_recall / len(test_dl))
+        return total_em / len(test_dl), total_mv / len(test_dl)
+
+    @staticmethod
+    def scoring_function(model, pre_seq, seq):
+        seq_pred = model(pre_seq).detach()
+        prediction = model.anomaly_detector(seq_pred.numpy(), seq.numpy(), model.anomaly_threshold)
+        return prediction
