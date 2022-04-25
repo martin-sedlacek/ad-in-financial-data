42c21ec3448a324f06593f428f9467407452b495
Martin Sedlacek <martin.martin.sedlacek@gmail.com>
2022-04-25 08:24:46 +0100

    MAD-GAN

--- models/MADGAN.py
+++ models/MADGAN.py
@@ -1,111 +1,50 @@
 import torch
 from torch import nn
-from pathlib import Path
-from typing import Optional, Union
 import torch.nn.functional as F
 
 
+# ***************************************************************************************
+# The following MAD-GAN implementation was inspired by the source code from Guillem96 (2022) with varying alterations
+# Availability: https://github.com/Guillem96/madgan-pytorch
+# ***************************************************************************************
 class Generator(nn.Module):
-    def __init__(self, latent_space_dim: int, hidden_units: int, output_dim: int, n_lstm_layers: int = 2) -> None:
+    def __init__(self, input_dim, hidden_size, output_dim, num_layers=2):
         super().__init__()
-        self.latent_space_dim = latent_space_dim
-        self.hidden_units = hidden_units
-        self.n_lstm_layers = n_lstm_layers
+        self.input_dim = input_dim
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
         self.output_dim = output_dim
 
-        self.lstm = nn.LSTM(input_size=self.latent_space_dim,
-                            hidden_size=self.hidden_units,
-                            num_layers=self.n_lstm_layers,
-                            batch_first=True,
-                            dropout=.1)
-
-        self.linear = nn.Linear(in_features=self.hidden_units,
-                                out_features=self.output_dim)
+        self.lstm = nn.LSTM(input_size=self.input_dim, hidden_size=self.hidden_size, num_layers=self.num_layers, dropout=0.2)
+        self.linear = nn.Linear(in_features=self.hidden_units, out_features=self.output_dim)
 
         nn.init.trunc_normal_(self.linear.bias)
         nn.init.trunc_normal_(self.linear.weight)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        rnn_output, _ = self.lstm(x)
-        return self.linear(rnn_output)
-
-    def save(self, fpath: Union[Path, str]) -> None:
-        chkp = {
-            "config": {
-                "latent_space_dim": self.latent_space_dim,
-                "hidden_units": self.hidden_units,
-                "n_lstm_layers": self.n_lstm_layers,
-                "output_dim": self.output_dim
-            },
-            "weights": self.state_dict(),
-        }
-        torch.save(chkp, fpath)
-
-    @classmethod
-    def from_pretrained(
-            cls,
-            fpath: Union[Path, str],
-            map_location: Optional[torch.device] = None) -> "Generator":
-        chkp = torch.load(fpath, map_location=map_location)
-        model = cls(**chkp.pop("config"))
-        model.load_state_dict(chkp.pop("weights"))
-        model.eval()
-        return model
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        return self.linear(out)
 
 
 class Discriminator(nn.Module):
-    def __init__(self, input_dim, hidden_units, n_lstm_layers: int = 2, add_batch_mean: bool = True) -> None:
+    def __init__(self, input_dim, hidden_size, num_layers=2):
         super().__init__()
-        self.add_batch_mean = add_batch_mean
-        self.hidden_units = hidden_units
+        self.hidden_units = hidden_size
         self.input_dim = input_dim
-        self.n_lstm_layers = n_lstm_layers
+        self.num_layers = num_layers
 
-        extra_features = self.hidden_units if self.add_batch_mean else 0
-        self.lstm = nn.LSTM(input_size=self.input_dim,
-                            hidden_size=self.hidden_units + extra_features,
-                            num_layers=self.n_lstm_layers,
-                            batch_first=True,
-                            dropout=.1)
+        # batch_first=True,
+        # extra_feature
+        self.lstm = nn.LSTM(input_size=self.input_dim, hidden_size=self.hidden_units, num_layers=self.num_layers, dropout=0.2)
+        self.linear = nn.Linear(in_features=self.hidden_units, out_features=1)
+        self.activation = nn.Sigmoid()
 
-        self.linear = nn.Linear(in_features=self.hidden_units + extra_features,
-                                out_features=1)
         nn.init.trunc_normal_(self.linear.bias)
         nn.init.trunc_normal_(self.linear.weight)
 
-        self.activation = nn.Sigmoid()
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        if self.add_batch_mean:
-            bs = x.size(0)
-            batch_mean = x.mean(0, keepdim=True).repeat(bs, 1, 1)
-            x = torch.cat([x, batch_mean], dim=-1)
-
-        rnn_output, _ = self.lstm(x)
-        return self.activation(self.linear(rnn_output))
-
-    def save(self, fpath: Union[Path, str]) -> None:
-        chkp = {
-            "config": {
-                "add_batch_mean": self.add_batch_mean,
-                "hidden_units": self.hidden_units,
-                "input_dim": self.input_dim,
-                "n_lstm_layers": self.n_lstm_layers
-            },
-            "weights": self.state_dict(),
-        }
-        torch.save(chkp, fpath)
-
-    @classmethod
-    def from_pretrained(
-            cls,
-            fpath: Union[Path, str],
-            map_location: Optional[torch.device] = None) -> "Discriminator":
-        chkp = torch.load(fpath, map_location=map_location)
-        model = cls(**chkp.pop("config"))
-        model.load_state_dict(chkp.pop("weights"))
-        model.eval()
-        return model
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        return self.activation(self.linear(out))
 
 
 class AnomalyDetector(object):
@@ -124,34 +63,31 @@ class AnomalyDetector(object):
         self.res_weight = res_weight
         self.DEVICE = DEVICE
 
-    def predict(self, tensor: torch.Tensor) -> torch.Tensor:
+    def predict(self, tensor):
         return (self.predict_proba(tensor) > self.threshold).int()
 
-    def predict_proba(self, tensor: torch.Tensor) -> torch.Tensor:
+    def predict_proba(self, tensor):
         discriminator_score = self.compute_anomaly_score(tensor)
         discriminator_score *= 1 - self.res_weight
         reconstruction_loss = self.compute_reconstruction_loss(tensor)
         reconstruction_loss *= self.res_weight
-        return discriminator_score #+ reconstruction_loss
+        return discriminator_score + reconstruction_loss
 
-    def compute_anomaly_score(self, tensor: torch.Tensor) -> torch.Tensor:
+    def compute_anomaly_score(self, tensor):
         with torch.no_grad():
             discriminator_score = self.discriminator(tensor)
         return discriminator_score
 
-    def compute_reconstruction_loss(self,
-                                    tensor: torch.Tensor) -> torch.Tensor:
+    def compute_reconstruction_loss(self, tensor):
         best_reconstruct = self._generate_best_reconstruction(tensor)
         return (best_reconstruct - tensor).abs().sum(dim=(1, 2))
 
-    def _generate_best_reconstruction(self, tensor: torch.Tensor) -> None:
+    def _generate_best_reconstruction(self, tensor: torch.Tensor):
         # The goal of this function is to find the corresponding latent space for the given
         # input and then generate the best possible reconstruction.
         max_iters = 10
 
-        Z = torch.empty(
-            (tensor.size(0), tensor.size(1), self.latent_space_dim),
-            requires_grad=True, device=self.DEVICE)
+        Z = torch.empty((tensor.size(0), tensor.size(1), self.latent_space_dim), requires_grad=True, device=self.DEVICE)
         nn.init.normal_(Z, std=0.05)
 
         optimizer = torch.optim.RMSprop(params=[Z], lr=0.1)
@@ -162,8 +98,7 @@ class AnomalyDetector(object):
             optimizer.zero_grad()
             generated_samples = self.generator(Z)
             normalized_input = F.normalize(generated_samples, dim=1, p=2)
-            reconstruction_error = loss_fn(normalized_input,
-                                           normalized_target).sum(dim=(0, 1, 2))
+            reconstruction_error = loss_fn(normalized_input, normalized_target).sum(dim=(0, 1, 2))
             reconstruction_error.backward()
             optimizer.step()
 
--- training/MADGAN_train.py
+++ training/MADGAN_train.py
@@ -1,37 +1,31 @@
 import numpy as np
 import torch
 import random
-from typing import Dict
-from pathlib import Path
 from models.MADGAN import AnomalyDetector
 from utils.evaluation import excess_mass, mass_volume
+from utils.evaluation import accuracy, precision, recall, metric_calc
 
 
 class MadGanTrainingPipeline():
-    def set_seed(self, seed: int = 0) -> None:
+    def set_seed(self, seed=0):
         torch.manual_seed(seed)
         np.random.seed(seed)
         random.seed(seed)
 
-    # --- sample Z from latent space --- #
-    def sample_Z(self, batch_size, seq_length, latent_dim, use_time=False):
+    def sample_Z(self, batch_size, seq_length, latent_dim):
         sample = np.float32(np.random.normal(size=[batch_size, seq_length, latent_dim]))
-        if use_time:
-            print('WARNING: use_time has different semantics')
-            sample[:, :, 0] = np.linspace(0, 1.0 / seq_length, num=seq_length)
         return torch.Tensor(sample)
 
-    def train_epoch(self, G, D, loss_fn, real_dl, G_optimizer, D_optimizer, seq_length, latent_dim, DEVICE,
-                    normal_label: int = 0, anomaly_label: int = 1, epoch: int = 0, log_every: int = 30) -> None:
+    '''
+    Training
+    '''
+    def train_epoch(self, D, G, loss_fn, train_dl, G_optimizer, D_optimizer, seq_length, latent_dim, DEVICE,
+                    normal_label=0, anomaly_label=1, epoch=0):
         G.train()
         D.train()
-        metric_accum = {
-            "generator_loss": 0,
-            "discriminator_loss_real": 0,
-            "discriminator_loss_fake": 0,
-        }
-        batch_count = 0
-        for i, (X, Y) in enumerate(real_dl):
+        g_loss_total = d_loss_real_total = d_loss_fake_total = 0
+
+        for i, (X, Y) in enumerate(train_dl):
             bs = X.size(0)
 
             # Samples
@@ -43,7 +37,7 @@ class MadGanTrainingPipeline():
             real_labels = torch.full((bs, seq_length, 1), normal_label).float().to(DEVICE)
             fake_labels = torch.full((bs, seq_length, 1), anomaly_label).float().to(DEVICE)
 
-            # Discriminator update
+            # Train D
             D_optimizer.zero_grad()
             real_d = D(real_samples)
             fake_d = D(fake_samples.detach())
@@ -55,7 +49,7 @@ class MadGanTrainingPipeline():
 
             D_optimizer.step()
 
-            # Genertor update
+            # train G
             G_optimizer.zero_grad()
             fake_d = D(fake_samples)
             g_loss = loss_fn(fake_d.view(-1), real_labels.view(-1))
@@ -63,38 +57,64 @@ class MadGanTrainingPipeline():
             G_optimizer.step()
 
             # Save metrics
-            metric_accum['generator_loss'] += g_loss.item()
-            metric_accum['discriminator_loss_real'] += d_loss_real.item()
-            metric_accum['discriminator_loss_fake'] += d_loss_fake.item()
-            batch_count += 1
+            g_loss_total += g_loss.item()
+            d_loss_real_total += d_loss_real.item()
+            d_loss_fake_total += d_loss_fake.item()
         D.zero_grad()
         G.zero_grad()
-        out = 'G_loss: ' + str(metric_accum['generator_loss'] / batch_count) + ', ' + 'D_loss_real: ' + str(
-            metric_accum['discriminator_loss_real'] / batch_count) + ', ' + 'D_loss_fake: ' + str(
-            metric_accum['discriminator_loss_fake'] / batch_count)
-        print('Epoch ' + str(epoch) + ' training:')
-        print(out)
+        print("Epoch {0}: G_loss: {1}, D_loss_real: {2}, D_loss_fake: {3}".format(epoch, g_loss_total / len(train_dl),
+                    d_loss_real_total / len(train_dl), d_loss_fake_total / len(train_dl)))
 
-    def train(self, seq_length, latent_dim, tscv_dl_list, D, G, D_optim, G_optim, loss_fn, random_seed,
-              num_epochs, DEVICE, model_dir: Path = Path("models/madgan")) -> None:
+    def train_financial(self, seq_length, latent_dim, tscv_dl_list, D, G, D_optim, G_optim, loss_fn, random_seed, num_epochs, DEVICE) -> None:
         self.set_seed(random_seed)
 
         ad = AnomalyDetector(discriminator=D, generator=G, latent_space_dim=latent_dim, anomaly_threshold=0.5, DEVICE=DEVICE)
-
         for train_dl, test_dl in tscv_dl_list:
             for epoch in range(num_epochs):
                 self.train_epoch(G, D, loss_fn, train_dl, G_optim, D_optim, seq_length, latent_dim, DEVICE,
                                  normal_label=0, anomaly_label=1, epoch=epoch)
-            self.evaluate(G, D, loss_fn, test_dl, seq_length, latent_dim, DEVICE, normal_label=0, anomaly_label=1)
             total_em = total_mv = 0
             for X, Y in test_dl:
-                scores = self.MAD_GAN_EMMV(ad, X.to(DEVICE), DEVICE=DEVICE)
-                total_em += scores['em']
-                total_mv += scores['mv']
+                em, mv = self.emmv(ad, X.to(DEVICE), DEVICE=DEVICE)
+                total_em += em
+                total_mv += mv
             print("EMMV evaluation:")
             print(total_em / len(test_dl), total_mv / len(test_dl))
 
-    def MAD_GAN_EMMV(self, trained_model, x, n_generated=10000, alpha_min=0.9, alpha_max=0.999, t_max=0.9, DEVICE="cpu"):
+    def train_kdd99(self, seq_length, latent_dim, train_dl, test_dl, D, G, D_optim, G_optim, loss_fn, random_seed, num_epochs, DEVICE) -> None:
+        self.set_seed(random_seed)
+
+        for epoch in range(num_epochs):
+            self.train_epoch(D, G, loss_fn, train_dl, G_optim, D_optim, seq_length, latent_dim, DEVICE, epoch=epoch)
+        ad = AnomalyDetector(discriminator=D, generator=G, latent_space_dim=latent_dim, anomaly_threshold=0.5, DEVICE=DEVICE)
+        self.evaluate(ad, test_dl, label=1, DEVICE=DEVICE)
+
+    '''
+    Evaluation
+    '''
+    def evaluate(self, model, test_dl, label, DEVICE):
+        total_em = total_mv = total_acc = total_pre = total_rec = 0
+        for X, Y in test_dl:
+            prediction = model.predict(X.to(DEVICE))
+            true_positives, true_negatives, false_positives, false_negatives = metric_calc(prediction.squeeze(), Y.squeeze(), label)
+            total_acc += accuracy(true_positives, true_negatives, Y)
+            if (true_positives+false_positives) > 0:
+                total_pre += precision(true_positives, false_positives)
+            if (true_positives+false_negatives) > 0:
+                total_rec += recall(true_positives, false_negatives)
+            em, mv = self.emmv(model, X.to(DEVICE), DEVICE=DEVICE)
+            total_mv += mv
+            total_em += em
+        print("Acc: {0}, Pre: {1}, Rec: {2}".format(total_acc/len(test_dl), total_pre/len(test_dl), total_rec/len(test_dl)))
+        print("EM: {0}, MV: {1}".format(total_em/len(test_dl), total_mv/len(test_dl)))
+
+    # ***************************************************************************************
+    # This method is an adaptation inspired by O'leary (2022) under the MIT open license.
+    # Availability: https://github.com/christian-oleary/emmv
+    # Note: the fundamental logic is not changed, but the pytorch implementation and customisation to support the
+    # model associated with this training pipeline is novel.
+    # ***************************************************************************************
+    def emmv(self, trained_model, x, n_generated=10000, alpha_min=0.9, alpha_max=0.999, t_max=0.9, DEVICE="cpu"):
         # Get limits and volume support.
         lim_inf = torch.min(x.view(-1, x.size(-1)), dim=0)[0]
         lim_sup = torch.max(x.view(-1, x.size(-1)), dim=0)[0]
@@ -126,55 +146,4 @@ class MadGanTrainingPipeline():
         AUC_em, em, amax = excess_mass(t, t_max, volume_support, s_unif_total, anomaly_score, n_generated)
         AUC_mv, mv = mass_volume(axis_alpha, volume_support, s_unif_total, anomaly_score, n_generated)
 
-        # Return a dataframe containing EMMV information
-        scores = {
-            'em': np.mean(em),
-            'mv': np.mean(mv),
-        }
-        return scores
-
-    @torch.no_grad()
-    def evaluate(self, G, D, loss_fn, real_dl, seq_length, latent_dim, DEVICE, normal_label: int = 0, anomaly_label: int = 1) -> Dict[str, float]:
-        metric_accum = {
-            "D_loss": 0,
-            "G_acc": 0,
-            "D_acc": 0
-        }
-        batch_count = 0
-        for X, Y in real_dl:
-            bs = X.size(0)
-
-            # Samples
-            real_samples = X.to(DEVICE)
-            latent_samples = self.sample_Z(bs, seq_length, latent_dim).to(DEVICE)
-            fake_samples = G(latent_samples)
-
-            # Labels
-            real_labels = torch.full((bs, seq_length, 1), normal_label).float().to(DEVICE)
-            fake_labels = torch.full((bs, seq_length, 1), anomaly_label).float().to(DEVICE)
-            all_labels = torch.cat([real_labels, fake_labels])
-
-            # Try to classify the real and generated samples
-            real_d = D(real_samples)
-            fake_d = D(fake_samples.detach())
-            all_d = torch.cat([real_d, fake_d]).to(DEVICE)
-
-            # Discriminator tries to identify the true nature of each sample (anomaly or normal)
-            d_real_loss = loss_fn(real_d.view(-1), real_labels.view(-1))
-            d_fake_loss = loss_fn(fake_d.view(-1), fake_labels.view(-1))
-            d_loss = d_real_loss + d_fake_loss
-
-            discriminator_acc = ((all_d > .5) == all_labels).float()
-            discriminator_acc = discriminator_acc.sum().div(2 * bs * seq_length)
-
-            generator_acc = ((fake_d > .5) == real_labels).float()
-            generator_acc = generator_acc.sum().div(bs * seq_length)
-
-            metric_accum["D_loss"] += d_loss.item()
-            metric_accum["D_acc"] += discriminator_acc.item()
-            metric_accum["G_acc"] += generator_acc.item()
-            batch_count += 1
-        for key in metric_accum.keys():
-            metric_accum[key] = metric_accum[key] / batch_count
-        print("Evaluation metrics:", metric_accum)
-        return metric_accum
+        return np.mean(em), np.mean(mv)
