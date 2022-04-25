34e0063fc96bf57ab2bf0c25efd16ccf96b41c32
Martin Sedlacek <martin.martin.sedlacek@gmail.com>
2022-04-25 08:24:26 +0100

    Edits to DeepAnT

--- models/DeepAnT.py
+++ models/DeepAnT.py
@@ -1,20 +1,24 @@
 import torch
 
+# ***************************************************************************************
+# The following DeepAnT_CNN implementation was inspired by the source code from Bmonikraj (2020) with alterations
+# Availability: https://github.com/bmonikraj/medium-ds-unsupervised-anomaly-detection-deepant-lstmae
+# ***************************************************************************************
 class DeepAnT_CNN(torch.nn.Module):
-    def __init__(self, LOOKBACK_SIZE, DIMENSION, anomaly_threshold):
+    def __init__(self, lookback_dim, output_dim, kernel_size, hidden_dim, num_channels, anomaly_threshold):
         super(DeepAnT_CNN, self).__init__()
         self.anomaly_threshold = anomaly_threshold
-        self.conv1d_1_layer = torch.nn.Conv1d(in_channels=LOOKBACK_SIZE, out_channels=64, kernel_size=1)
+        self.conv1d_1_layer = torch.nn.Conv1d(in_channels=lookback_dim, out_channels=num_channels, kernel_size=kernel_size)
         self.relu_1_layer = torch.nn.ReLU()
         self.maxpooling_1_layer = torch.nn.MaxPool1d(kernel_size=2)
-        self.conv1d_2_layer = torch.nn.Conv1d(in_channels=64, out_channels=64, kernel_size=1)
+        self.conv1d_2_layer = torch.nn.Conv1d(in_channels=num_channels, out_channels=num_channels, kernel_size=kernel_size)
         self.relu_2_layer = torch.nn.ReLU()
         self.maxpooling_2_layer = torch.nn.MaxPool1d(kernel_size=2)
         self.flatten_layer = torch.nn.Flatten()
-        self.dense_1_layer = torch.nn.Linear(64, 40)
+        self.dense_1_layer = torch.nn.Linear(hidden_dim, 40) #64
         self.relu_3_layer = torch.nn.ReLU()
         self.dropout_layer = torch.nn.Dropout(p=0.25)
-        self.dense_2_layer = torch.nn.Linear(40, DIMENSION)
+        self.dense_2_layer = torch.nn.Linear(40, output_dim)
 
     def forward(self, x):
         x = self.conv1d_1_layer(x)
@@ -33,19 +37,19 @@ class DeepAnT_CNN(torch.nn.Module):
     @staticmethod
     def anomaly_detector(prediction_seq, ground_truth_seq, anm_det_thr):
         # calculate Euclidean between actual seq and predicted seq
-        dist = torch.absolute(ground_truth_seq - prediction_seq).mean(dim=prediction_seq.dim()-1)
+        dist = torch.sqrt(torch.square(ground_truth_seq - prediction_seq)).mean(dim=prediction_seq.dim()-1)
         return (dist > anm_det_thr).to(dtype=torch.float32)
 
 
 class DeepAnT_LSTM(torch.nn.Module):
-    def __init__(self, dim, hidden_dim, layers, anomaly_threshold):
+    def __init__(self, lookback_dim, hidden_dim, num_layers, anomaly_threshold):
         super(DeepAnT_LSTM, self).__init__()
         self.anomaly_threshold = anomaly_threshold
-        self.lstm_1_layer = torch.nn.LSTM(dim, hidden_dim, layers)
+        self.lstm_1_layer = torch.nn.LSTM(lookback_dim, hidden_dim, num_layers)
         self.dropout_1_layer = torch.nn.Dropout(p=0.2)
-        self.lstm_2_layer = torch.nn.LSTM(hidden_dim, 64, layers)
+        self.lstm_2_layer = torch.nn.LSTM(hidden_dim, 64, num_layers)
         self.dropout_2_layer = torch.nn.Dropout(p=0.2)
-        self.linear_layer = torch.nn.Linear(64, dim)
+        self.linear_layer = torch.nn.Linear(64, lookback_dim)
 
     def forward(self, x):
         x, (_, _) = self.lstm_1_layer(x)
@@ -53,6 +57,37 @@ class DeepAnT_LSTM(torch.nn.Module):
         x, (_, _) = self.lstm_2_layer(x)
         x = self.dropout_2_layer(x)
         return self.linear_layer(x)
+
+    @staticmethod
+    def anomaly_detector(prediction_seq, ground_truth_seq, anm_det_thr):
+        # calculate Euclidean between actual seq and predicted seq
+        dist = torch.absolute(ground_truth_seq - prediction_seq).mean(dim=prediction_seq.dim()-1)
+        return (dist > anm_det_thr).to(dtype=torch.float32)
+
+
+class Extended_DeepAnT_LSTM(torch.nn.Module):
+    def __init__(self, lookback_dim, hidden_dim, input_seq_len, output_seq_len, num_layers, anomaly_threshold):
+        super(Extended_DeepAnT_LSTM, self).__init__()
+        self.expansion_factor = 4
+        self.anomaly_threshold = anomaly_threshold
+        self.lstm_1_layer = torch.nn.LSTM(lookback_dim, hidden_dim, num_layers)
+        self.dropout_1_layer = torch.nn.Dropout(p=0.2)
+        self.lstm_2_layer = torch.nn.LSTM(hidden_dim, 64, num_layers)
+        self.dropout_2_layer = torch.nn.Dropout(p=0.2)
+        self.linear_layer = torch.nn.Linear(64, lookback_dim)
+        self.mlp_layer_1 = torch.nn.Linear(input_seq_len, self.expansion_factor*input_seq_len)
+        self.mlp_layer_2 = torch.nn.Linear(self.expansion_factor*input_seq_len, output_seq_len)
+
+    def forward(self, x):
+        x, (_, _) = self.lstm_1_layer(x)
+        x = self.dropout_1_layer(x)
+        x, (_, _) = self.lstm_2_layer(x)
+        x = self.dropout_2_layer(x)
+        x = self.linear_layer(x)
+        x = x.transpose(1, 2)
+        x = self.mlp_layer_1(x)
+        x = self.mlp_layer_2(x)
+        return x.transpose(1, 2)
 
     @staticmethod
     def anomaly_detector(prediction_seq, ground_truth_seq, anm_det_thr):
--- training/DeepAnT_train.py
+++ training/DeepAnT_train.py
@@ -1,15 +1,18 @@
 import torch
-from utils import evaluation
 import numpy as np
+from utils.evaluation import excess_mass, mass_volume, metric_calc, accuracy, precision, recall
 
 
 class DeepAntTrainingPipeline():
+    '''
+    Training
+    '''
     @staticmethod
     def train_epoch(model, loss_fn, train_dl, optimizer, DEVICE):
         loss_sum = 0.0
-        for X, Y in train_dl:
+        for (X, Y, next_steps, next_labels) in train_dl:
             X = X.to(device=DEVICE, dtype=torch.float)
-            next_step_seq = Y.squeeze().to(device=DEVICE, dtype=torch.float)
+            next_step_seq = next_steps.squeeze().to(device=DEVICE, dtype=torch.float)
             model.train()
             pred_next = model(X).squeeze().to(device=DEVICE, dtype=torch.float)
             loss = loss_fn(next_step_seq, pred_next)
@@ -27,16 +30,57 @@ class DeepAntTrainingPipeline():
                 print('Epoch {0} loss: {1}'.format(epoch, epoch_loss))
 
             tmp_em = tmp_mv = 0
-            for X, Y in test_dl:
-                scores = self.deepant_emmv(model, X.to(DEVICE), Y.to(DEVICE), DEVICE=DEVICE)
-                tmp_em += scores['em']
-                tmp_mv += scores['mv']
+            for (X, Y, next_steps, next_labels) in test_dl:
+                em, mv = self.deepant_emmv(model, X.to(DEVICE), next_steps.to(DEVICE), DEVICE=DEVICE)
+                tmp_em += em
+                tmp_mv += mv
             print("EMMV evaluation:")
             print(tmp_em / len(test_dl), tmp_mv / len(test_dl))
             total_em += tmp_em / len(test_dl)
             total_mv += tmp_mv / len(test_dl)
         print('Final results - EM: {0} MV: {1}'.format(total_em/len(tscv_dl_list), total_mv/len(tscv_dl_list)))
 
+    def train_kdd99(self, train_dl, test_dl, model, optimizer, loss, num_epochs, DEVICE):
+        for epoch in range(num_epochs):
+            epoch_loss = self.train_epoch(model, loss, train_dl, optimizer, DEVICE)
+            print('Epoch {0} loss: {1}'.format(epoch, epoch_loss))
+        self.evaluate(model, test_dl, 1, DEVICE)
+
+    def train_kdd99_full(self, train_dl_normal, train_dl_anomaly, test_dl_normal, test_dl_anomaly, model, optimizer, loss, num_epochs, DEVICE):
+        for epoch in range(num_epochs):
+            epoch_loss = self.train_epoch(model, loss, train_dl_normal, optimizer, DEVICE)
+            epoch_loss += self.train_epoch(model, loss, train_dl_anomaly, optimizer, DEVICE)
+            print('Epoch {0} loss: {1}'.format(epoch, epoch_loss/2))
+        self.evaluate(model, test_dl_anomaly, 1, DEVICE)
+        self.evaluate(model, test_dl_normal, 0, DEVICE)
+    '''
+    Evaluation
+    '''
+    @torch.no_grad()
+    def evaluate(self, model, test_dl, label, DEVICE):
+        model.eval()
+        total_em = total_mv = total_acc = total_pre = total_rec = 0
+        for (X, Y, next_steps, next_labels) in test_dl:
+            seq_prediction = model(X.to(DEVICE)).detach().unsqueeze(dim=1)
+            prediction = model.anomaly_detector(seq_prediction, next_steps.to(DEVICE), model.anomaly_threshold)
+            true_positives, true_negatives, false_positives, false_negatives = metric_calc(prediction, next_labels, label)
+            total_acc += accuracy(true_positives, true_negatives, next_labels)
+            if (true_positives + false_positives) > 0:
+                total_pre += precision(true_positives, false_positives)
+            if (true_positives + false_negatives) > 0:
+                total_rec += recall(true_positives, false_negatives)
+            em, mv = self.deepant_emmv(model, X.to(DEVICE), next_steps.to(DEVICE), DEVICE=DEVICE)
+            total_mv += mv
+            total_em += em
+        print("Acc: {0}, Pre: {1}, Rec: {2}".format(total_acc / len(test_dl), total_pre / len(test_dl), total_rec / len(test_dl)))
+        print("EM: {0}, MV: {1}".format(total_em / len(test_dl), total_mv / len(test_dl)))
+
+    # ***************************************************************************************
+    # This method is an adaptation inspired by O'leary (2022) under the MIT open license.
+    # Availability: https://github.com/christian-oleary/emmv
+    # Note: the fundamental logic is not changed, but the pytorch implementation and customisation to support the
+    # model associated with this training pipeline is novel.
+    # ***************************************************************************************
     @staticmethod
     def deepant_emmv(trained_model, pre_x, x, n_generated=10000, alpha_min=0.9, alpha_max=0.999, t_max=0.9, DEVICE="cpu"):
         def scoring_function(model, pre_seq, seq):
@@ -77,12 +121,7 @@ class DeepAntTrainingPipeline():
         anomaly_score = scoring_function(trained_model, pre_x, x).view(-1, 1).detach().cpu().numpy()
 
         # Get EM and MV scores
-        AUC_em, em, amax = evaluation.excess_mass(t, t_max, volume_support, s_unif_total, anomaly_score, n_generated)
-        AUC_mv, mv = evaluation.mass_volume(axis_alpha, volume_support, s_unif_total, anomaly_score, n_generated)
+        AUC_em, em, amax = excess_mass(t, t_max, volume_support, s_unif_total, anomaly_score, n_generated)
+        AUC_mv, mv = mass_volume(axis_alpha, volume_support, s_unif_total, anomaly_score, n_generated)
 
-        # Return a dataframe containing EMMV information
-        scores = {
-            'em': np.mean(em),
-            'mv': np.mean(mv),
-        }
-        return scores
+        return np.mean(em), np.mean(mv)
