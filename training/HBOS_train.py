805db1744453c7f8f369fa426b8371a4c3599ce1
Martin Sedlacek <martin.martin.sedlacek@gmail.com>
2022-04-28 04:33:50 +0100

    Add HBOS

---
+++ training/HBOS_train.py
@@ -0,0 +1,61 @@
+import torch
+from utils.evaluation import excess_mass, mass_volume, metric_calc, accuracy, precision, recall
+import numpy as np
+
+
+def train_kdd99(model, train_x, train_y, test_x, test_y):
+    model.fit(train_x)
+    prediction = model.predict(test_x)
+    true_positives, true_negatives, false_positives, false_negatives = metric_calc(torch.tensor(prediction).unsqueeze(dim=1), torch.tensor(test_y).unsqueeze(dim=1), 1)
+    acc = accuracy(true_positives, true_negatives, torch.tensor(test_y).unsqueeze(dim=1))
+    pre = precision(true_positives, false_positives) if (true_positives + false_positives) > 0 else 0.0
+    rec = recall(true_positives, false_negatives) if (true_positives + false_negatives) > 0 else 0.0
+    print("Acc: {0}, Pre: {1}, Rec: {2}".format(acc, pre, rec))
+    em, mv = hbos_emmv(model, test_x)
+    print("EM: {0}, MV: {1}".format(em, mv))
+
+
+def train(model, tscv_list):
+    total_em = total_mv = 0
+    for X, Y, next_steps, next_labels in tscv_list:
+        X = X.squeeze()
+        model.fit(X)
+        em, mv = hbos_emmv(model, X)
+        total_em += em
+        total_mv += mv
+    print("EM: {0}, MV: {1}".format(total_em/len(tscv_list), total_mv/len(tscv_list)))
+
+# ***************************************************************************************
+# This method is provided by O'leary (2022) under the MIT open license. Small adjustments were made to fit this script.
+# Availability: https://github.com/christian-oleary/emmv
+# ***************************************************************************************
+def hbos_emmv(trained_model, x, n_generated=10000, alpha_min=0.9, alpha_max=0.999, t_max=0.9):
+    # Get limits and volume support.
+    lim_inf = x.min(axis=0)
+    lim_sup = x.max(axis=0)
+    offset = 1e-60  # to prevent division by 0
+
+    # Volume support
+    try:
+        volume_support = (lim_sup - lim_inf).prod() + offset
+    except AttributeError as e:
+        volume_support = (lim_sup - lim_inf) + offset
+
+    # Determine EM and MV parameters
+    t = np.arange(0, 100 / volume_support, 0.01 / volume_support)
+    axis_alpha = np.arange(alpha_min, alpha_max, 0.0001)
+
+    try:
+        unif = np.random.uniform(lim_inf, lim_sup, size=(n_generated, x.shape[1]))
+    except IndexError as e:
+        unif = np.random.uniform(lim_inf, lim_sup, size=(n_generated))
+
+    # Get anomaly scores
+    anomaly_score = trained_model.predict(x)
+    s_unif = trained_model.predict(unif)
+
+    # Get EM and MV scores
+    AUC_em, em, amax = excess_mass(t, t_max, volume_support, s_unif, anomaly_score, n_generated)
+    AUC_mv, mv = mass_volume(axis_alpha, volume_support, s_unif, anomaly_score, n_generated)
+
+    return np.mean(em), np.mean(mv)
